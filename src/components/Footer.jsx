ee5a4ece68934de3bdd8bfd01f0b8858aba81507
facundo1996 <ffaacu1@gmail.com>
2024-04-12 12:11:27 -0300

    Contact responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,11 +16,11 @@ export default function Footer() {
 
   return (
     <div className='footer-container row'>
-      <div className='d-flex flex-column justify-content-center align-items-center col-6 b-r-2'>
+      <div className='d-flex flex-column justify-content-center align-items-center col-12 col-xl-6 b-r-2'>
         <img className='logo-footer' src="/icons/logo-completo.png" alt="Sistema constructivo" />
-        <div className='text-md c-light mt-5'>LÍDERES EN CONSTRUCCIÓN INDUSTRIALIZADA</div>
+        <div className='text-md c-light mt-4 mt-xl-5'>LÍDERES EN CONSTRUCCIÓN INDUSTRIALIZADA</div>
       </div>
-      <div className='d-flex flex-column justify-content-center align-items-start col-6 social-footer'>
+      <div className='d-flex flex-column justify-content-center align-items-center align-items-xl-start col-12 col-xl-6 social-footer'>
         <a href='/' onClick={(e) => scrollToComponent(e, 'homepage')}>HOME </a>
         <a href='/' onClick={(e) => scrollToComponent(e, 'system')}>SISTEMA CONSTRUCTIVO AMZ </a>
         <a href='/' onClick={(e) => scrollToComponent(e, 'commercialization')}>COMERCIALIZACIÓN </a>
