03b60114688e55eb81cbf6b2155d1ff76d9a27ab
facundo1996 <ffaacu1@gmail.com>
2024-04-24 17:04:42 -0300

    Fix Construction System

--- src/components/ConstructionSystem.jsx
+++ src/components/ConstructionSystem.jsx
@@ -4,11 +4,11 @@ export default function ConstructionSystem() {
   return (
     <div id='system' className='d-flex row construction-system-container'>
 
-      <div className='col-12 col-xxl-6 d-flex justify-content-xxl-center align-items-center title-system'>
+      <div className='col-12 col-xl-6 d-flex justify-content-xl-center align-items-center title-system'>
         <h1>SISTEMA <br /> CONSTRUCTIVO AMZ</h1>
       </div>
 
-      <div className='col-12 col-xxl-6 text-construction-system'>
+      <div className='col-12 col-xl-6 text-construction-system'>
 
         <div className=''>
           Nuestro método constructivo representa una <b>solución a las problemáticas inherentes a la construcción tradicional</b>, al combinar tecnología de vanguardia, procesos eficientes, y la alta calidad y beneficios del hormigón celular. Nuestra propuesta no solo <b>optimiza los tiempos de ejecución y reduce los costos asociados</b>, sino que también <b>garantiza una mayor durabilidad, seguridad y sustentabilidad</b> en cada proyecto.
